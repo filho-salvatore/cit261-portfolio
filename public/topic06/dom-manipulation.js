d913708790d36b4a6c86cbbd7b7b3a51541e9229
Rasputtim <rasputtim@gmx.com>
2020-02-01 20:31:36 -0300

    DOM Code

---
+++ public/topic06/dom-manipulation.js
@@ -0,0 +1,116 @@
+/**
+ * Create and add the remove button to each email in the list
+ * 
+ */
+var myLIlist = document.getElementsByTagName("LI");
+var i;
+
+for (i = 0; i < myLIlist.length; i++) {
+
+  let mySpan = document.createElement("SPAN");
+  let txt = document.createTextNode("  [\u00D7] ");
+  mySpan.className = "remove";
+  mySpan.appendChild(txt);
+  myLIlist[i].appendChild(mySpan);
+
+}
+
+/**
+ *  Click on a remove button to hide the current list item
+ */
+var remove = document.getElementsByClassName("remove");
+var i;
+
+for (i = 0; i < remove.length; i++) {
+
+  remove[i].onclick = function() {
+
+    let parent = this.parentElement;
+    parent.style.display = "none";
+
+  };
+
+}
+
+
+/**
+ * Add e-mail to the CC list
+ */
+function newCCEmail() {
+
+ 
+  let inputValue = document.getElementById("myCCEmail").value;
+  let li = document.createElement("li");
+  let text = document.createTextNode(inputValue);
+
+  li.appendChild(text);
+
+  if (inputValue === '') {
+
+    alert("missing the e-mail to add");
+
+  } else {
+
+    document.getElementById("myCC").appendChild(li);
+
+  }
+
+  document.getElementById("myCCEmail").value = "";
+
+  let mySpan = document.createElement("SPAN");
+  let txt = document.createTextNode(" [\u00D7] ");
+  mySpan.className = "remove";
+  mySpan.appendChild(txt);
+  li.appendChild(mySpan);
+
+  for (i = 0; i < remove.length; i++) {
+
+    remove[i].onclick = function() {
+
+      let parent = this.parentElement;
+      parent.style.display = "none";
+
+    };
+  }
+}
+
+
+
+/**
+ * Add e-mail to the CCO list
+ */
+function newCCOEmail() {
+
+  
+  let inputValue = document.getElementById("myCCOEmail").value;
+  let li = document.createElement("li");
+  let text = document.createTextNode(inputValue);
+  li.appendChild(text);
+
+  if (inputValue === '') {
+
+    alert("Missing the e-mail to add!");
+
+  } else {
+    document.getElementById("myCCO").appendChild(li);
+  }
+
+  document.getElementById("myCCOEmail").value = "";
+
+  let mySpan = document.createElement("SPAN");
+  let txt = document.createTextNode(" [\u00D7] ");
+  mySpan.className = "remove";
+  mySpan.appendChild(txt);
+  li.appendChild(mySpan);
+
+  for (i = 0; i < remove.length; i++) {
+
+    remove[i].onclick = function() {
+
+      let parent = this.parentElement;
+      parent.style.display = "none";
+
+    };
+
+  }
+}
---
+++ public/topic06/dom.js
@@ -0,0 +1,75 @@
+/**
+ * TOPIC 06 DOM
+ * 
+ */
+
+
+document.getElementById("showDOM").addEventListener("click", function() {
+    document.getElementById("result").innerHTML ="";
+    document.getElementById("result").innerHTML = showDOMTree(nodeTree);
+});
+/**
+ *  Show Node Tree from the some point
+ */
+document.getElementById("show_DOM_TREE_Element").addEventListener("click", function() {
+    document.getElementById("result").innerHTML ="";
+    var elementTree = null;
+    let startElement = document.getElementById("startPointInTree").value;
+    let element  = document.getElementsByTagName(startElement);
+    for (let i = 0; i < element.length; i++) {
+        elementTree = getNodeTree(element[i]);
+        document.getElementById("result").innerHTML += showDOMTree(elementTree);
+    }
+});
+
+
+ 
+
+/**
+ * Create the DOM tree in the nodeTree Variable
+ */
+var nodeTree = getNodeTree(document.documentElement);
+
+
+function getNodeTree(node) {
+    if (node.hasChildNodes()) {
+        var children = [];
+        for (var j = 0; j < node.childNodes.length; j++) {
+            children.push(getNodeTree(node.childNodes[j]));
+        }
+
+        return {
+            nodeName: node.nodeName,
+            parentName: node.parentNode.nodeName,
+            children: children,
+            content: node.innerText || "",
+        };
+    }
+
+    return false;
+}
+
+function showDOMTree(node) {
+	if (!node) return "";
+    
+    var txt = "";
+	
+    if (node.children.length > 0) {
+        txt += "<ul><li><b>Node:</b> " + node.nodeName + "</li>";
+        txt += "<li> <b>Parent:</b> " + node.parentName + "</li>";
+        txt += "<li><b>Content:</b> " + node.content + "</li>";
+        for (var i = 0; i < node.children.length; i++)
+        	if (node.children[i])
+            	txt += "<li> <b>Children:</b> " + showDOMTree(node.children[i]) + "</li>";
+        txt += "</ul>";
+    }/*else{
+        txt += "<ul><li>Nodo: " + node.nodeName + "</li>";
+        txt += "<li> Padre: " + node.parentNode.nodeName + "</li>";
+        txt += "<li>Contenido: " + node.content + "</li>";
+    }*/
+
+    return txt;
+}
+
+
+
