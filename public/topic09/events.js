349faeb6d9ef4cb5477ab2ee6499d1c50e57c3f4
Rasputtim <rasputtim@gmx.com>
2020-02-10 14:08:59 -0300

    topic08 added

---
+++ public/topic09/events.js
@@ -0,0 +1,53 @@
+/**
+ * On Touch event for mobiles
+ */
+
+addEventListener('touchmove', function(e) { // When the user moves their finger, the function will begin.
+     
+    e.preventDefault(); // Stop any annoying scrolling.
+ 
+    var touch = e.touches[0]; // Fires every time the user moves.
+         
+    var posY = touch.pageY - 25; // Get the Y position (-25 to center)
+    var posX = touch.pageX - 25; // Get the X position (-25 to center)
+     
+    if(e.touches.length == 1) { // If one finger is touching
+     
+        // Creates div at current position of finger 
+        // -----------------------------------------
+         
+        var blue = document.createElement('div');
+        blue.setAttribute('class', 'blue');
+         
+        blue.style.top = posY+'px';
+        blue.style.left = posX+'px';
+         
+        document.body.appendChild(blue); 
+         
+    }
+     
+    if(e.touches.length == 2) { // If two fingers are touching
+     
+        var purple = document.createElement('div');
+        purple.setAttribute('class', 'purple');
+         
+        purple.style.top = posY+'px';
+        purple.style.left = posX+'px';
+         
+        document.body.appendChild(purple);
+     
+    }
+     
+    if(e.touches.length == 3) { // If three fingers are touching
+     
+        var red = document.createElement('div');
+        red.setAttribute('class', 'red');
+         
+        red.style.top = posY+'px';
+        red.style.left = posX+'px';
+         
+        document.body.appendChild(red);
+     
+    }
+     
+}, false);
