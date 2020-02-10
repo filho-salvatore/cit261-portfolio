0bba723cdb3cf07f33b8d1e7e84fa0521b030564
Rasputtim <rasputtim@gmx.com>
2020-02-10 13:14:37 -0300

    topic08 added

--- public/topic08/animations.js
+++ public/topic08/animations.js
@@ -1,27 +1,46 @@
-let theZoomPic = document.getElementById('zoomPic');
-let zoomBGSize = window.getComputedStyle(theZoomPic).getPropertyValue('background-size');
+let theHertPicture = document.getElementById('heartPic');
+let zoomBGSize = window.getComputedStyle(theHertPicture).getPropertyValue('background-size');
     
-
+/**
+ * vent Handler to give zoom to the picture
+ */
 document.getElementById('zoom').onclick = function() {
-  if(!theZoomPic.classList.contains('zoom'))
+  if(!theHertPicture.classList.contains('zoom'))
   {
-    theZoomPic.classList.add('zoom');
+    theHertPicture.classList.add('zoom');
   } 
-}
-document.getElementById('pause').onclick = function() {
-    let computedStyle = window.getComputedStyle(theZoomPic),
-        backgroundSize = computedStyle.getPropertyValue('background-size');
-    theZoomPic.style.backgroundSize = backgroundSize;
-    theZoomPic.classList.remove('zoom');
-  }
-document.getElementById('zoomout').onclick = function() {
-  theZoomPic.classList.remove('zoom');
-  theZoomPic.style.backgroundSize= zoomBGSize;
 }
 
 /**
- * Animated Heart
+ * Event Handler to pause the zoom transform
  */
+document.getElementById('pause').onclick = function() {
+    let computedStyle = window.getComputedStyle(theHertPicture),
+        backgroundSize = computedStyle.getPropertyValue('background-size');
+    theHertPicture.style.backgroundSize = backgroundSize;
+    theHertPicture.classList.remove('zoom');
+  }
 
+  /**
+   * Event Handler to remove the zoom from the picture
+   */
+  document.getElementById('zoomout').onclick = function() {
+  theHertPicture.classList.remove('zoom');
+  theHertPicture.style.backgroundSize= zoomBGSize;
+}
 
+/**
+ * Event Handler to stop the heart animation
+ */
+stopbtn.addEventListener("click", function(){
+	heart.style.animation = "infinite";
+	heart.style.webkitAnimation = "infinite";
+});
 
+/**
+ * Event Handler to start the heart animation
+ */
+startbtn.addEventListener("click", function(){
+	heart.style.animation = "";
+	heart.style.webkitAnimation = "";
+});
