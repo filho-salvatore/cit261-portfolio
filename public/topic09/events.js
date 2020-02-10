a3a054686913154ec57a38ee0df7aaec59f40440
Rasputtim <rasputtim@gmx.com>
2020-02-10 16:28:43 -0300

    topic09 added

--- public/topic09/events.js
+++ public/topic09/events.js
@@ -62,13 +62,85 @@ window.addEventListener('load', function(){
 
 }, false);
 
-function clear(){
+/**
+ * mouse events
+ */
 
-    let blueElements=document.getElementsByClassName('blue');
-    blueElements.forEach(function (item, index) {
-        document.body.removeChild(item);
-      });
-    
+ // When true, moving the mouse draws on the canvas
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myPics = document.getElementById('myPics');
+const context = myPics.getContext('2d');
+
+// The x and y offset of the canvas from the edge of the page
+const rect = myPics.getBoundingClientRect();
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+myPics.addEventListener('mousedown', e => {
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+  isDrawing = true;
+});
+
+myPics.addEventListener('mousemove', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+  }
+});
+
+window.addEventListener('mouseup', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = 'black';
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
 }
 
+
+/**
+ * Animation events
+ */
+
+let square = document.getElementById('square');
+let animIterations = 0;
+let cancel= document.getElementById('cancelAnimation');
+let start= document.getElementById('startAnimation');
+
+start.addEventListener('click', function(ev) {
+ 
+    square.style.animation = "";
+});
+
+cancel.addEventListener('click', function(ev) {
+    //square.style.animationName="test";
+    
+  square.style.animation = "test";
+  square.style.webkitAnimation = "test";
+  });
+
+
+square.addEventListener('animationstart', (ev) => {
+  alert("Animation Startted", ev);
+});
+
+square.addEventListener('animationiteration', (ev) => {
+    let countSquare = document.getElementById('count');
+    animIterations ++;
+    countSquare.innerHTML = "Animation Iterations: "+ animIterations;
+});
 
