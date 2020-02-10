dc97a876de7826eda749c4d742a5090b89c0894a
Rasputtim <rasputtim@gmx.com>
2020-02-10 15:37:42 -0300

    topic09 added

--- public/topic09/events.js
+++ public/topic09/events.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function(){
                 var posX = touch.pageX - 25; // Get the X position (-25 to center)
                 
                 if(posY <= boxCoord.top)  posY = boxCoord.top ;
-                if(posY >= boxCoord.bottom)  posY = boxCoord.bottom - 25;
+                if(posY >= boxCoord.bottom)  posY = boxCoord.bottom - 50;
 
                 if(e.touches.length == 1) { // If one finger is touching
                 
